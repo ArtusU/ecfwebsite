b846a25f6ced80e5e2c4f67e56e00ff4c4d8ac7c
ArtusU <artusrock@hotmail.com>
2020-08-27 15:06:07 +0100

    contact.html

--- website/views.py
+++ website/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+
+def blog(request):
+    return render(request, 'blog.html', {})
